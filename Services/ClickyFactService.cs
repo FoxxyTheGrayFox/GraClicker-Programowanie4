01b1339040e2237cca9e796419cb72c7534df59b
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2026-04-26 11:34:30 +0200

    Even more useless facts

--- Services/ClickyFactService.cs
+++ Services/ClickyFactService.cs
@@ -10,7 +10,10 @@ namespace ProjektClicker
         "Zapalniczka została wynaleziona 3 lata przed zapałkami.",
         "W Finlandii jest najwięcej zespołów heavy metalowych per capita.",
         "Ważki mimo posiadania 6 odnóży nie potrafią chodzić.",
-        "Banan jest klasyfikowany jako jagoda."
+        "Banan jest klasyfikowany jako jagoda.",
+        "Papież Jan Paweł II został mianowany honorowym członkiem drużyny Harlem Globetrotters.",
+        "Na każdym kontynencie, z wyjątkiem Antarktydy, znajduje się miasto o nazwie „Rzym”.",
+        "Królowa Elżbieta II była mechanikiem z wykształcenia."
     ];
     public string GetRandomFact() => _facts[_rand.Next(_facts.Length)];
 }
